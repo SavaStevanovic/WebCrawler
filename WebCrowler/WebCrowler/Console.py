fff61b897b05b2553b2a645b9b0a505f5d8ec022
SavaStevanovic <sava.stevanovic@pmf.edu.rs>
2017-09-13 16:20:06 +0200

    Program can now be stoped by executing command Stop, and program is restarted if new Start command is executed

--- WebCrowler/WebCrowler/Console.py
+++ WebCrowler/WebCrowler/Console.py
@@ -5,30 +5,35 @@ Created on Sep 9, 2017
 '''
 #Start http://www.pmf.ni.ac.rs/pmf/index.php pmf
 #Start https://www.wikipedia.org/ wikipe
+#Stop
 
-started=True;
 from WebCrowler import WebCrowler
 import time
 
+def sss(webCrawler, command):
+    commandParts = [part.strip(' ') for part in command.split(' ')]
+    if (commandParts[0] == 'Start'):
+        if (webCrawler != None):
+            webCrawler.processesShutDown()
+        webCrawler = WebCrowler(commandParts[1], commandParts[2].split(','))
+        webCrawler.processesEngage()
+    if (commandParts[0] == 'Stop'):
+        if (webCrawler != None):
+            webCrawler.processesShutDown()
+    return webCrawler
 
-
-def processCommand(command):
-    commandParts=[part.strip(' ') for part in command.split(' ')]
-    val={
-        'Start':WebCrowler(commandParts[1],commandParts[2].split(',')).processesEngage(),
-        #'Stop':WebCrowler(commandParts[1]).processesShutDown()
-        }['Start']
-    
-    
 
 if __name__ == '__main__':
+    started=True
+    webCrawler=None
     while started:
         try:
             command=input(">:");
-            processCommand(command)
+            webCrawler = sss(webCrawler, command)
             print('Program started.')
             time.sleep(0.5)
         except KeyboardInterrupt:  
-            started=False
+            if(webCrawler!=None):
+                webCrawler.processesShutDown()
         
         
